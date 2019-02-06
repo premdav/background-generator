b10c01f20b7c722a80dbe10c6b4a6ae808120733
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-06 13:48:26 -0600

    adding starting project files

---
+++ main.js
@@ -0,0 +1,34 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
+var body = document.getElementById("gradient");
+var button = document.querySelector("button");
+
+function changeBackground() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value; + ")";
+    css.textContent = body.style.background + ";";
+}
+
+function randomHex() {
+    var letters = '0123456789ABCDEF';
+    var genColor1 = '#';
+    var genColor2 = '#';
+    
+    for (var i = 0; i < 6; i++) {
+        genColor1 += letters[Math.floor(Math.random() * 16)];
+    }
+    for (var i = 0; i < 6; i++) {
+        genColor2 += letters[Math.floor(Math.random() * 16)];
+    }
+    color1.value = genColor1;
+    color2.value = genColor2;
+    changeBackground();
+
+}
+
+window.onload = changeBackground();
+
+color1.addEventListener("input", changeBackground);
+color2.addEventListener("input", changeBackground);
+button.addEventListener("click", randomHex);
+

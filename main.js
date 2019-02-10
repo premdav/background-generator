a0d7f060a44b6c1c379035698d4b308e73035385
Preston Davis <35507470+premdav@users.noreply.github.com>
2019-02-10 10:44:02 -0600

    changed to es6 arrow functions

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-var _ = require('lodash');
+let _ = require('lodash');
 
-var array = [1, 2, 3, 4, 5, 6, 7, 8];
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log('answer:', _.without(array, 4));
-var css = document.querySelector("h3");
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
-var body = document.getElementById("gradient");
-var button = document.querySelector("button");
+let css = document.querySelector("h3");
+let color1 = document.querySelector("#color1");
+let color2 = document.querySelector("#color2");
+let body = document.getElementById("gradient");
+let button = document.querySelector("button");
 
-function changeBackground() {
+const changeBackground = () => {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value; + ")";
     css.textContent = body.style.background + ";";
 }
 
-function randomHex() {
-    var letters = '0123456789ABCDEF';
-    var genColor1 = '#';
-    var genColor2 = '#';
+const randomHex = () =>  {
+    let letters = '0123456789ABCDEF';
+    let genColor1 = '#';
+    let genColor2 = '#';
     
-    for (var i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         genColor1 += letters[Math.floor(Math.random() * 16)];
     }
-    for (var i = 0; i < 6; i++) {
+    for (let i = 0; i < 6; i++) {
         genColor2 += letters[Math.floor(Math.random() * 16)];
     }
     color1.value = genColor1;
